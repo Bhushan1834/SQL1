15224008599e49a27dd5281caf27378cb43cf60e
Bhushan tarte <bhushantarte44@gmail.com>
2026-01-21 16:18:22 +0530

    Update SQLQuery4.sql

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -11,13 +11,13 @@ grade INT
 )
 
 INSERT INTO sstudents(student_iD,name,age,grade)
-VALUES(101,'Raju',10,5)
+VALUES(101,'Raju',10,7)
 
 INSERT INTO sstudents(student_iD,name,age,grade)
-VALUES(101,'Raju',10,5)
+VALUES(102,'Baburao',15,5)
 
 INSERT INTO sstudents(student_iD,name,age,grade)
-VALUES(102,'Sham',16,7)
+VALUES(103,'Sham',16,7)
 
 update sstudents
 set grade=6
@@ -27,6 +27,49 @@ select *from sstudents
 
 --Q2.Add a new student to the table 
 
---Q3.Write a query to remove 'baburao' from the table 
---Q4.Write a query to retrive only yhe details for the student named'Sham'
---Q5.Write a query to print/get age of Raju 
+create database bhushan
+use bhushan
+
+CREATE TABLE sstudents(
+student_id INT,
+name VARCHAR(100),
+age INT,
+grade INT
+)
+
+insert into sstudents(student_id,name,age,grade)
+values(104,'Alex',11,6)
+
+select * from sstudents
+
+
+--Q3.Write a query to remove 'baburao' from the table
+create database bhushan
+use bhushan
+
+CREATE TABLE sstudents(
+student_id INT,
+name VARCHAR(100),
+age INT,
+grade INT
+)
+
+INSERT INTO sstudents(student_iD,name,age,grade)
+VALUES(102,'Baburao',15,5)
+
+delete from sstudents
+where name= 'Baburao'
+
+select * from sstudents
+
+--Q4.Write a query to retrive only the details for the student named'Sham'
+
+SELECT *
+FROM sstudents
+WHERE Name = 'Sham';
+
+--Q5.Write a query to print/get age of Raju
+
+select age 
+from sstudents
+where name='Raju';
